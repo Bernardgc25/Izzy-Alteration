7846a4c9a3a354a5a375909092eac0f4036d6125
Bernardgc25 <Bernard_gcruz@yahoo.com>
2026-02-07 21:57:19 -0800

    clean up node modules (global)

--- test/setup.js
+++
@@ -1,6 +0,0 @@
-import { register } from 'node:module'
-import { pathToFileURL } from 'node:url'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-register('testdouble', pathToFileURL(__filename))
